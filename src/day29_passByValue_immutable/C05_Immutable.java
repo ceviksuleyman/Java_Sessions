2fc2d42078d917e39c307f730844e90d82220972
Suleyman Cevik <you@example.com>
2022-07-23 16:20:51 +0300

    23.07.22

--- src/day29_passByValue_immutable/C05_Immutable.java
+++ src/day29_passByValue_immutable/C05_Immutable.java
@@ -4,9 +4,16 @@ public class C05_Immutable {
 
     public static void main(String[] args) {
 
+        /*
+        NOTE ==> Immutable class'dan olusturulan objeler de immutable olurlar.
+        Ornek String str, str String class'indan bir objedir ...
+        Bu tur bir obje olusturabiliriz fakat onlari degistiremeyiz.
+        Immutable objeyi degistirmek istersek , Java o objeyi klonlar ve yapilan degisiklikleri klonlanmis
+        yeni obje uzerinde gerceklestirir.
+         */
         String isim = "Mine";
 
-        isim = isim.toUpperCase(); // isim kalici olarak degisti
+        isim = isim.toUpperCase(); // isim kalici olarak degisti yeni obje olusturulmus oldu assign var
 
         System.out.println(isim); // MİNE
 
