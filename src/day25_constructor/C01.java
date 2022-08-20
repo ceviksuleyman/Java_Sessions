37f0abc7a810c8ac997903a621513ac49e194e03
cevik <suleymancevik64@gmail.com>
2022-08-21 02:46:05 +0300

    THE END

--- src/day25_constructor/C01.java
+++ src/day25_constructor/C01.java
@@ -8,6 +8,7 @@ public class C01 {
 
        Ama her class'dan object uretilmeyebilir,bunun icin ihtiyac halinde kullanilmasi icin her class'da default
     bir Constructor koymustur.
+
        Bu default constructor class'da obje olusturuldugunda otomatik olarak calisir.
        Ornegin bu class'da constructor gorunmemesine ragmen C02 class'inda icinde oldugumuz C01 class'indan
     bir object uretebildik. bkz.C02_Constructor
@@ -20,8 +21,10 @@ public class C01 {
                           // =============== OZET OLARAK =================
         /*
         1-Bizim bir object olusturabilmemiz icin mutlaka bir Constructor'a ihtiyacimiz vardir
+
         2-Biz constructor'a ihtiyacimiz olan ozel parametreleri yazmazsak,Java bizim icin standart bir urun uretir
         ve onu bize yollar.Istedigimiz spesifik ozellikler varsa bunlari parametreler vasitasiyla soylememiz gereklidir.
+
         3-Bir class'da gorunurde bir constructor yoksa bile aslinda Java oraya bir default constructor koymustur,
         bu ihtiyac halinde class'dan bir obje olusturuldugunda otomatik olarak default constructor'i kullanir.
          */
--- src/day26_constructor/Araba.java
+++ src/day26_constructor/Araba.java
@@ -2,23 +2,6 @@ package day26_constructor;
 
 public class Araba {
 
-    /*-------------------------------- Onceki Dersten Aklimizda Kalanlar -----------------------------------------
-      1- Constructor : Java'nin obje olusturmak icin kullandigi kod blogudur.Bir constructor calismadan, obje olusmasi
-      mumkun degildir.
-      2- Bir class olusturdugumuzda, java o class'tan obje olusturulmasini saglamak icin her class'a default constructor
-      koyar.Default constructor gorunmez,parametresi yoktur,constructor body'sinde extra bir kod yoktur.
-      3- Biz constructor'dan spesifik ozelliklere sahip bir obje istiyorsak bunu parametre ile constructor'a iletmemiz
-      veya objeyi base ozelliklerde olusturup sonra istedigimiz degerleri atamamiz gerekir.
-      4- Biz de constructor olusturabiliriz,urettigimiz constructor ister parametreli, isterse de default constructor
-      gibi parametresiz olsun,biz olusturdugumuzda Java default constructor'i siler.
-      5- Her obje olusturuldugunda, obje olusturulan class'daki temel atamalara sahip olur. Bir objenin herhangi bir
-      ozelliginin degerini bulmak istedigimizde Java su siralamaya bakar;
-          1) Obje olusturulduktan sonra deger atanmis mi ?
-          2) 1. madde yoksa ,obje olusturulan class'a gidip o variable'a atanmis bir deger var mi?
-          3) 2 secenek de yoksa Java'nin data turune gore belirledigi default degeri alir.
-      6- Constructor ismi class ismi ile ayni olmalidir,costructor isminden sonra () ve varsa parametreler,
-      ve sonra da {} constructor body olmalidir.
-       */
     public String marka = "Marka Belirtilmedi.";
     public String model = "Model Belirtilmedi.";
 
